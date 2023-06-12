00408e63d38903d4c70da2dede7aa8d1058c14a8
MorshedAl <123994547+MorshedAl@users.noreply.github.com>
2023-06-12 20:36:05 +0600

    Add files via upload

---
+++ Class_Object.py
@@ -0,0 +1,35 @@
+
+#class(design/blueprint) and object(real বস্তু)
+
+class student:
+ roll=" "
+ gpa=" "
+ subject=""
+
+raju=student()
+raju.roll=50
+raju.gpa=3.50
+raju.subject="economics"
+
+
+raj=student()
+raj.roll=70
+raj.gpa=2.99
+raj.subject="physics"
+
+print(f'raju roll:{raju.roll} gpa={raju.gpa} subject={raju.subject}')
+print(f'raj roll:{raj.roll} gpa={raj.gpa} subject={raj.subject}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
