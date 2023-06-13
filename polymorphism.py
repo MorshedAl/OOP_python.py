5642e7db8f74680a4aa86738736c9d6bd405e37a
MorshedAl <123994547+MorshedAl@users.noreply.github.com>
2023-06-13 11:14:51 +0600

    Add files via upload

---
+++ polymorphism.py
@@ -0,0 +1,20 @@
+#polymorphism
+
+
+# built-in polymorphism 
+print(len('Bangladesh'))
+print(len([10,15,20,30]))
+
+
+# user defined polymorphism 
+
+def add(x,y,z=0):
+ return x+y+z
+
+
+m=add(5,10)
+print(m)
+
+p=add(5,10,15)
+print(p)
+
